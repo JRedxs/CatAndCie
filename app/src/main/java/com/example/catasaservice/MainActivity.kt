48c4f02ca425e936c96c6f2c44c967d4aceceba8
Vonkuro <vonkuro@gmail.com>
2023-02-06 16:00:21 +0100

    exploration of user action on spinner

--- app/src/main/java/com/example/catasaservice/MainActivity.kt
+++ app/src/main/java/com/example/catasaservice/MainActivity.kt
@@ -2,18 +2,21 @@ package com.example.catasaservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    var menuState: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setUpMenu()
     }
 
-
-    fun setUpMenu(){
+    private fun setUpMenu(){
         val menu: Spinner = findViewById(R.id.spinner_menu)
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter.createFromResource(
@@ -26,5 +29,19 @@ class MainActivity : AppCompatActivity() {
             // Apply the adapter to the spinner
             menu.adapter = adapter
         }
+        menu.onItemSelectedListener = this
     }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+        val console: TextView = findViewById(R.id.Console_textView)
+        console.setText(parent.selectedItem.toString())
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        // Another interface callback
+        // Do Nothing
+    }
+
 }
